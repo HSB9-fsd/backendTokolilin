e7b1b9e2342b1392cb183125a57582ec382a640e
21abdurr <abdurr223@gmail.com>
2024-01-16 19:05:00 +0700

    Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # backendTokolilin
+
+## installation
+
+Install All the depedencies using npm
+
+    npm install
+
+Or
+
+if you using yarn
+
+    yarn
+
+Copy the env-example file and make the required configuration changes in the .env file
+
+    cp env-example .env
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+
 const router = require('./routes/router');
 const handleError = require('../middlewares/handleError');
 
@@ -15,4 +16,4 @@ app.use(cors({ origin: true, credentials: true }));
 app.use('/', router);
 app.use(handleError);
 
-app.listen(process.env.SERVER_PORT, () => {console.log('Oke Sip Lanjut')});
+app.listen(process.env.SERVER_PORT, () => { console.log('Oke Sip Lanjut') });
